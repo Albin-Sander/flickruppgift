0ba70c9f05b8d1446c64859636ef826f1d6cda3e
Albin <albinsander17@outlook.com>
2019-11-10 16:44:21 +0100

    new things
    Added new things

--- script/script.js
+++ script/script.js
@@ -21,26 +21,66 @@ catchDisney()
 });
 
 async function catchDisney() {
-    const response = await fetch('https:farm66.staticflickr.com/65535/49017181663_80997984c3_m.jpg')
+    const response = await fetch('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=19d3e6e0acfe9c438f368e2c2bab1c5d&tags=disney&format=json&nojsoncallback=1')
     const blob = await response.blob();
     document.getElementById('disney').src = URL.createObjectURL(blob);
+    console.log(catchDisney);
 }
 
+$.ajax({
+    url: 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=19d3e6e0acfe9c438f368e2c2bab1c5d&tags=disney&format=json&nojsoncallback=1',
+    type: 'GET',
+    dataType: "json",
+    success: displayAll
+});
 
-
-/*farm-id: 66
-server-id: 65535
-photo-id: 49017181663
-secret: 80997984c3
-size: m*/
-
-
+function displayAll(data){
     
-
-
-
+    let jsonData = data.photos.photo;
     
+    for (i = 0; i < jsonData.length; i++) {
         
-            
+        console.log(jsonData[i]);
+        let jsonFarmId = data.photos.photo[i].farm;
+        console.log(jsonFarmId);
+        let jsonServerId = data.photos.photo[i].server;
+        console.log(jsonServerId);
+        let jsonId = data.photos.photo[i].id;
+        console.log(jsonId);
+        let jsonSecret = data.photos.photo[i].secret;
+        console.log(jsonSecret);
+
+
+        let pictureUrl = 'http://farm' + jsonFarmId + '.static.flickr.com/' + jsonServerId + '/' +jsonId + '_' +jsonSecret + '.jpg'
+        console.log(pictureUrl);
+
+        
+        }
+        }
+
         
     
+
+
+    
+
+
+
+
+
+
+
+
+
+/*let jsonId = data.photos.photo[0].id;
+for (i = 0; i < jsonId.length; i++) {
+    console.log(jsonId[i]);
+}
+
+*/
+
+
+
+
+
+
